563f4f3909d26ad5c2f21e83f6fdfc6d3b2e697d
Alexandre Bencz <alebencz@gmail.com>
2012-10-09 22:20:47 -0300

    Start the scanner... (:
    Signed-off-by: Alexandre Bencz <alebencz@gmail.com>

--- Compiler-study/Source/scanner.cpp
+++ Compiler-study/Source/scanner.cpp
@@ -5,5 +5,379 @@
 
 const char* const TOKEN_DESCRIPTION[] =
 {
-	"IDENTIFIER"
+	"IDENTIFIER",
+	"RESERVED_WORD",
+	"INT_CONST",
+	"REAL_CONST",
+	"STR_CONST",
+	"STRING_CONST",
+	"OPERATION",
+	"DELIMITER",
+	"END_OF_FILE"
 };
+
+// ************************
+// Reserved Words
+// ************************
+
+void ReservedWords::Add(char *value, TokenType type)
+{
+	words.insert(pair<char*, TokenType>(value, type));
+}
+
+ReservedWords::ReservedWords()
+{
+	Add("absolute", RESERVED_WORD);
+	Add("absolute", RESERVED_WORD);
+    Add("and", OPERATION);
+    Add("array", RESERVED_WORD);
+    Add("asm", RESERVED_WORD);
+    Add("begin", RESERVED_WORD);
+    Add("case", RESERVED_WORD);
+    Add("const", RESERVED_WORD);
+    Add("constructor", RESERVED_WORD);
+    Add("destructor", RESERVED_WORD);
+    Add("div", OPERATION);
+    Add("do", RESERVED_WORD);
+    Add("downto", RESERVED_WORD);
+    Add("else", RESERVED_WORD);
+    Add("end", RESERVED_WORD);
+    Add("file", RESERVED_WORD);
+    Add("for", RESERVED_WORD);
+    Add("function", RESERVED_WORD);
+    Add("goto", RESERVED_WORD);
+    Add("if", RESERVED_WORD);
+    Add("implementation", RESERVED_WORD);
+    Add("in", RESERVED_WORD);
+    Add("inherited", RESERVED_WORD);
+    Add("inline", RESERVED_WORD);
+    Add("interface", RESERVED_WORD);
+    Add("label", RESERVED_WORD);
+    Add("mod", OPERATION);
+    Add("nil", RESERVED_WORD);
+    Add("not", OPERATION);
+    Add("object", OPERATION);
+    Add("of", RESERVED_WORD);
+    Add("or", OPERATION);
+    Add("on", RESERVED_WORD);
+    Add("operator", RESERVED_WORD);
+    Add("packed", RESERVED_WORD);
+    Add("procedure", RESERVED_WORD);
+    Add("program", RESERVED_WORD);
+    Add("record", RESERVED_WORD);
+    Add("repeat", RESERVED_WORD);
+    Add("reintroduce", RESERVED_WORD);
+    Add("set", RESERVED_WORD);
+    Add("self", RESERVED_WORD);
+    Add("shl", OPERATION);
+    Add("shr", OPERATION);
+    Add("string", RESERVED_WORD);
+    Add("then", RESERVED_WORD);
+    Add("to", RESERVED_WORD);
+    Add("type", RESERVED_WORD);
+    Add("unit", RESERVED_WORD);
+    Add("until", RESERVED_WORD);
+    Add("uses", RESERVED_WORD);
+    Add("var", RESERVED_WORD);
+    Add("while", RESERVED_WORD);
+    Add("with", RESERVED_WORD);
+    Add("xor", OPERATION);
+    Add("..", RESERVED_WORD);
+    Add(":=", OPERATION);
+    Add("-", OPERATION);
+    Add("+", OPERATION);
+    Add("*", OPERATION);
+    Add("/", OPERATION);
+    Add("[", OPERATION);
+    Add("]", OPERATION);
+    Add(";", DELIMITER);
+    Add(":", DELIMITER);
+    Add(",", DELIMITER);
+    Add(".", OPERATION);
+    Add("^", OPERATION);
+    Add("@", OPERATION);
+    Add("(", OPERATION);
+    Add(")", OPERATION);
+    Add(">", OPERATION);
+    Add("<", OPERATION);
+    Add("=", OPERATION);
+    Add(">=", OPERATION);
+    Add("<=", OPERATION);
+    Add("<>", OPERATION);
+}
+
+bool ReservedWords::Identify(char *value, TokenType& returned_type)
+{
+	map<string, TokenType>::iterator i = words.find(value);
+	if(i == words.end())
+		return false;
+	returned_type = i->second;
+	return true;
+}
+
+// ************************
+// Token
+// ************************
+
+ostream& operator<<(ostream& out, const Token& token)
+{
+	out << token.GetLine() << ':' << token.GetPos() << ' ' << TOKEN_DESCRIPTION[token.GetType()]
+		<< ' ' << token.GetValue() << endl;
+	return out;
+}
+
+Token::Token() : value(NULL) { }
+
+Token::Token(char *value, TokenType type, int line, int pos)
+{
+	this->value = strcpy(new char[strlen(value)+1], value);
+	this->type = type;
+	this->line = line;
+	this->pos = pos;
+}
+
+Token::Token(const Token& token)
+{
+	value = strcpy(new char[strlen(token.value)+1], token.value);
+	type = token.type;
+	line = token.line;
+	pos = token.pos;
+}
+
+Token& Token::operator=(const Token& token)
+{
+	if(value != NULL)
+		delete(value);
+	value = strcpy(new char[strlen(token.value)+1], token.value);
+	type = token.type;
+	line = token.line;
+	pos = token.pos;
+	return *this;
+}
+
+Token::~Token()
+{
+	delete(value);
+}
+
+TokenType Token::GetType() const
+{
+	return type;
+}
+
+const char* Token::GetValue() const
+{
+	return value;
+}
+
+int Token::GetPos() const
+{
+	return pos;
+}
+
+int Token::GetLine() const
+{
+	return line;
+}
+
+// ************************
+// Scanner
+// ************************
+
+Scanner::exception::exception() : msg(NULL) { }
+
+Scanner::exception::exception(const char* const msg) :
+	msg(strcpy(new char[strlen(msg) + 1], msg))
+{
+}
+
+const char* Scanner::exception::what() const throw()
+{
+	return msg;
+}
+
+// ************************
+// Scanner
+// ************************
+
+void Scanner::AddToBuffer(char c)
+{
+	buffer[bp] = c;
+	buffer_low[bp++] = tolower(c);
+}
+
+void Scanner::MakeToken(TokenType type)
+{
+	buffer[bp] = '\0';
+	token = Token(buffer, type, first_line, first_pos);
+	bp = 0;
+	state = NONE_ST;		 
+}
+
+void Scanner::IdentifyAndMake()
+{
+	buffer_low[bp] = '\0';
+	TokenType t;
+	if(!reserved_words.Identify(buffer_low, t))
+		t = IDENTIFIER;
+	MakeToken(t);
+}
+
+bool Scanner::TryToIdentify()
+{
+	buffer_low[bp] = '\0';
+	TokenType t;
+
+	if(reserved_words.Identify(buffer_low, t))
+	{
+		MakeToken(t);
+		return true;
+	}
+	else
+		return false;
+}
+
+void Scanner::Error(const char *msg) const
+{
+	stringstream s;
+	s << line << ':' << pos << " ERROR " << msg;
+	throw(Scanner::exception(s.str().c_str() ));
+}
+
+Scanner::Scanner(istream& input) : 
+	in(input),
+	pos(0),
+	line(1),
+	bp(0),
+	state(NONE_ST)
+{
+}
+
+Token Scanner::GetToken()
+{
+	return token;
+}
+
+Token Scanner::NextToken()
+{
+	while(ProcessNextCharacter());
+	return token;
+}
+
+void Scanner::EatLineComment(istream& in, char& curr_char)
+{
+	char& c = curr_char;
+	if(c == '/' && (char)in.peek() == '/')
+	{
+		do
+		{
+			c = ExtractChar();
+		} while (c != '\n' && !in.eof());
+	}
+}
+
+void Scanner::EatBlockComment(istream& in, char& curr_char)
+{
+	char& c = curr_char;
+	if(c == '{')
+	{
+		do
+		{
+			c = in.get();
+			++pos;
+			if(c == '\n')
+			{
+				++line;
+				pos = 0;
+			}
+			if(in.eof())
+				Error("end of file in comment");
+		} while (c != '}');
+		c = ExtractChar();
+	}
+}
+
+void Scanner::EatRealFractPart(istream& in, char& curr_char)
+{
+	char& c = curr_char;
+	while(isdigit(c))
+	{
+		AddToBuffer(c);
+		c = ExtractChar();
+	}
+	if(c == 'e' || c == 'E')
+	{
+		AddToBuffer(c);
+		c = ExtractChar();
+		if(c == '+' || c == '-')
+		{
+			AddToBuffer(c);
+			c = ExtractChar();
+		}
+		if(!isdigit(c))
+			Error("Illegal character, should be number");
+		else
+		{
+			do
+			{
+				AddToBuffer(c);
+				c = ExtractChar();
+			} while (isdigit(c));
+		}
+	}
+	MakeToken(REAL_CONST);
+}
+
+void Scanner::EatStrConst(istream& in, char& curr_char)
+{
+	char& c = curr_char;
+	--bp;
+	while(c != '\'')
+	{
+		if(in.eof())
+			Error("end of file in string");
+		if(c != '\n')
+			AddToBuffer(c);
+		else if(buffer[bp - 1] != '\\')
+			Error("end of line in string");
+		else
+		{
+			++line;
+			pos = 0;
+			--bp;
+		}
+		int count = 0;
+		c = in.get();
+		++pos;
+		while(c == '\'' && !in.eof())
+		{
+			++count;
+			if(count % 2 == 0)
+				AddToBuffer(c);
+			c = in.get();
+			++pos;
+		}
+		if(count % 2)
+			break;
+	}
+	if(!in.eof())
+	{
+		c = ExtractChar();
+	}
+	MakeToken(STR_CONST);
+}
+
+char Scanner::ExtractChar()
+{
+	++pos;
+	return in.get();
+}
+
+bool Scanner::ProcessNextCharacter()
+{
+	bool matched = false;
+
+	// ???? o.O
+
+	return !matched;
+}
--- Compiler-study/Source/scanner.h
+++ Compiler-study/Source/scanner.h
@@ -40,12 +40,34 @@ private:
 	char *value;
 public:
 	Token();
+	Token(char *value, TokenType type, int line, int pos);
+	Token(const Token& token);
+	Token& operator=(const Token& token);
+	~Token();
+	TokenType GetType() const;
+	const char* GetValue() const;
+	int GetPos() const;
+	int GetLine() const;
 };
+
+ostream& operator<<(ostream& out, const Token& token);
 
 class Scanner
 {
 public:
-	class exception: std::exception
+    static const int MAX_TOKEN_LENGTH = 256;
+    
+	enum State 
+	{
+        IDENTIFIER_ST,
+        INTEGER_ST,
+        REAL_FRACT_PART_ST,
+        OPERATION_ST,
+        EOF_ST,
+        NONE_ST
+    };
+
+    class exception : std::exception
 	{
     private:
         char *msg;
@@ -54,6 +76,32 @@ public:
         exception(const char* const msg);
         virtual const char* what() const throw();
     };
+
+private:
+    ReservedWords reserved_words;
+	Token* currentToken;
+	istream& in;
+	char buffer[MAX_TOKEN_LENGTH];
+	char buffer_low[MAX_TOKEN_LENGTH];
+	int bp;
+	int first_pos;
+	int first_line;
+	Token token;
+	int line;
+	int pos;
+	State state;
+	void AddToBuffer(char c);
+    void MakeToken(TokenType type);
+    void IdentifyAndMake();
+    bool TryToIdentify();
+    void Error(const char* msg) const;
+    char ExtractChar();
+    bool ProcessNextCharacter();
+    void EatLineComment(istream& in,char& curr_char);
+    void EatBlockComment(istream& in, char& curr_char);
+    void EatRealFractPart(istream& in, char& curr_char);
+    void EatStrConst(istream& in, char& curr_char);
+
 public:
 	Scanner(istream& input);
 	Token GetToken();
